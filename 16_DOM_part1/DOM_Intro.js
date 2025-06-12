d01214b00a19fb9f7be2830e0230d061710127e5
Ayan Das <ayandas62898@gmail.com>
2025-06-12 17:18:58 +0530

    Add files via upload

---
+++ 16_DOM_part1/DOM_Intro.js
@@ -0,0 +1,14 @@
+//  *******************  DOM  *******************
+                // Document Object Model
+/*
+        Document refers to the Web page html.
+
+            window Object
+            ducement Object
+        head                body
+    meta    title       div     p
+
+    document is used to manipulate the html
+*/
+
+document.getElementById('one').innerHTML='<h1> Hi I am Ayan</h1>';// Changes the innerHTML 
